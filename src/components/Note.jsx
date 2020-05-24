0d822f1af3b59a5684f7e2bfcfb8a5de3a1ca232
cortneyg <gardner.cortney@yahoo.com>
2020-05-23 22:08:08 -0600

    add files

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Note(){
+    return (
+    <div className="note">
+        <h1>Cortney</h1>
+        <p>I'm a Developer</p>
+    </div>
+    );
+}
+
+export default Note;
